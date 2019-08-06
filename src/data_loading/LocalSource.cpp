33e2e9b7bcc43bd075490eb5a6e5600f0f0e7b76
chris <christopher.t.bate1@gmail.com>
2019-08-06 10:03:07 -0600

    Add additional getBlob method to local source

--- src/data_loading/LocalSource.cpp
+++ src/data_loading/LocalSource.cpp
@@ -1,12 +1,24 @@
 #include "LocalSource.hpp"
 #include <fstream>
+#include <iostream>
 using namespace DLFS;
 
-std::vector<uint8_t> get_blob(const std::string &blob_path)
+LocalSource::LocalSource(const std::string &base_dir)
+{
+    m_baseDir = base_dir;
+}
+
+std::vector<uint8_t> LocalSource::get_blob(const std::string &blob_path)
 {
     unsigned int file_size = 0;
 
-    std::ifstream infile(blob_path, std::ifstream::binary);
+    std::string full_path = m_baseDir + blob_path;
+
+    std::ifstream infile(full_path, std::ifstream::binary);
+    if (!infile.is_open())
+    {
+        throw std::runtime_error("Failed to open file");
+    }
     infile.seekg(0, std::ios::end);
     file_size = infile.tellg();
     infile.seekg(0, std::ios::beg);
@@ -14,4 +26,29 @@ std::vector<uint8_t> get_blob(const std::string &blob_path)
     std::vector<uint8_t> buffer(file_size);
     infile.read(reinterpret_cast<char *>(buffer.data()),
                 buffer.size());
+
+    return buffer;
+}
+
+/**
+ * Reads in binary file and fills destBuffer
+ */
+void LocalSource::getBlob(const std::string &blobPath, std::vector<uint8_t> &destBuffer)
+{
+    unsigned int file_size = 0;
+
+    std::string full_path = m_baseDir + blobPath;
+
+    std::ifstream infile(full_path, std::ifstream::binary);
+    if (!infile.is_open())
+    {
+        throw std::runtime_error("Failed to open file");
+    }
+    infile.seekg(0, std::ios::end);
+    file_size = infile.tellg();
+    infile.seekg(0, std::ios::beg);
+
+    destBuffer.resize(file_size);
+    infile.read(reinterpret_cast<char *>(destBuffer.data()),
+                destBuffer.size());
 }
--- src/data_loading/LocalSource.hpp
+++ src/data_loading/LocalSource.hpp
@@ -10,7 +10,12 @@ namespace DLFS{
 class LocalSource : public DataSource
 {
 public:
+    LocalSource( const std::string &base_dir);
     std::vector<uint8_t> get_blob(const std::string &blob_path);
+
+    void getBlob(const std::string &blobPath, std::vector<uint8_t> &destBuffer);
+private:
+    std::string m_baseDir;
 };
 }
 
