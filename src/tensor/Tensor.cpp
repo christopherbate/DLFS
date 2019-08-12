ee4f23fc1a744ac65a505311d86b332b1c4b0b84
chris <christopher.t.bate1@gmail.com>
2019-08-11 20:26:44 -0600

    Dual grad tensor buffer handeling

--- src/tensor/Tensor.cpp
+++ src/tensor/Tensor.cpp
@@ -11,15 +11,10 @@ using namespace DLFS;
 using namespace std;
 
 template <typename T>
-Tensor<T>::Tensor() :
-    m_pitch{sizeof(T)}
+Tensor<T>::Tensor()
 {
-    m_bufferSize = 0;
     m_shape = {0, 0, 0, 0};
-    m_deviceBuffer = NULL;    
     m_dataType = CUDNN_DATA_FLOAT;
-    m_filterDesc = NULL;
-    m_tensorDesc = NULL;
 }
 
 template <typename T>
@@ -28,6 +23,8 @@ Tensor<T>::~Tensor()
     Deallocate();
     if (m_filterDesc)
         cudnnDestroyFilterDescriptor(m_filterDesc);
+    if (m_dwFilterDesc)
+        cudnnDestroyFilterDescriptor(m_dwFilterDesc);
     if (m_tensorDesc)
         cudnnDestroyTensorDescriptor(m_tensorDesc);
 }
@@ -55,8 +52,8 @@ void Tensor<T>::SetShape(int b, int h, int w, int c)
  * **/
 template <typename T>
 void Tensor<T>::SetFilterShape(int numInputChn,
-                            int numOutputChn,
-                            int rows, int cols)
+                               int numOutputChn,
+                               int rows, int cols)
 {
     m_isFilter = true;
     m_shape = {numOutputChn, rows, cols, numInputChn};
@@ -77,6 +74,16 @@ void Tensor<T>::FillCUDNNDesc()
         checkCudaErrors(cudnnSetFilter4dDescriptor(m_filterDesc, m_dataType,
                                                    CUDNN_TENSOR_NHWC, m_shape[0], m_shape[3],
                                                    m_shape[1], m_shape[2]));
+
+        if (m_calcGrad)
+        {
+            if (!m_dwFilterDesc)
+                checkCudaErrors(cudnnCreateFilterDescriptor(&m_dwFilterDesc));
+            checkCudaErrors(cudnnSetFilter4dDescriptor(m_dwFilterDesc,
+                                                       m_dataType, CUDNN_TENSOR_NHWC,
+                                                       m_shape[0], m_shape[3], m_shape[1],
+                                                       m_shape[2]));
+        }
     }
     else
     {
@@ -90,33 +97,46 @@ void Tensor<T>::FillCUDNNDesc()
 template <typename T>
 void Tensor<T>::AllocateIfNecessary()
 {
-    size_t needed_bytes = GetExpectedSize();
-    if (needed_bytes > m_bufferSize || m_deviceBuffer == NULL)
-    {
-        m_bufferSize = needed_bytes;
-        Allocate();
+    size_t needed_bytes = GetLinearSize()*sizeof(T);
+    bool needGradBuffer = m_calcGrad && m_deviceBufferGrad == nullptr;
+    if (needed_bytes > m_bufferSize || m_deviceBuffer == nullptr || needGradBuffer)
+    {   
+        m_bufferSize = needed_bytes;     
+        Allocate();        
     }
 }
 
 template <typename T>
 void Tensor<T>::Allocate()
 {
-    if (m_deviceBuffer != NULL)
-    {
-        Deallocate();
+    if (m_deviceBuffer != nullptr || m_deviceBufferGrad != nullptr)    
+        Deallocate();    
+
+    cout << "Allocating tensor " << GetName() << ":" << GetId() << " " << (float)m_bufferSize / 1024000.0 << " Mb" << std::endl;
+    checkCudaErrors(cudaMalloc(&m_deviceBuffer, m_bufferSize));    
+
+
+    if(m_calcGrad){
+        cout << "Allocating grad tensor " << GetName() << ":" << GetId() << " " << (float)m_bufferSize / 1024000.0 << " Mb" << std::endl;
+        checkCudaErrors(cudaMalloc(&m_deviceBufferGrad, m_bufferSize));
     }
-    cout << "Allocating tensor " << m_name << ":" << m_id << " " << (float)m_bufferSize/1024000.0 << " Mb"<< std::endl;
-    checkCudaErrors(cudaMalloc(&m_deviceBuffer, m_bufferSize));
 }
 
 template <typename T>
 void Tensor<T>::Deallocate()
 {
-    if (m_deviceBuffer != NULL)
+    if (m_deviceBuffer != nullptr)
     {
-        cout << "Attempting to deallocate Tensor " << m_name << ":" << m_id << endl;
+        cout << "Attempting to deallocate Tensor " << GetName() << ":" << GetId() << endl;
         checkCudaErrors(cudaFree(m_deviceBuffer));
-        m_deviceBuffer = NULL;
+        m_deviceBuffer = nullptr;
+        m_bufferSize = 0;        
+    }
+    if(m_deviceBufferGrad != nullptr)
+    {
+        cout << "Attempting to deallocate grad Tensor " << GetName() << ":" << GetId() << endl;
+        checkCudaErrors(cudaFree(m_deviceBufferGrad));
+        m_deviceBufferGrad = nullptr;        
     }
 }
 
@@ -129,16 +149,16 @@ TensorPtr<T> Tensor<T>::Convolve(TensorPtr<T> filter)
 
     // Create the output tensor.
     TensorShape outputShape = convOp->Prepare();
-    TensorPtr<T> outputTensor = ADContext.CreateTensor<T>();    
+    TensorPtr<T> outputTensor = ADContext.CreateTensor<T>();
     outputTensor->SetName("ConvOutput");
     outputTensor->SetShape(outputShape);
     outputTensor->AllocateIfNecessary();
-    
+
     convOp->SetOutput(outputTensor);
 
-    cout << "Conv op prepared, output shape: " << outputTensor->PrintShape() <<  endl;
+    cout << "Conv op prepared, output shape: " << outputTensor->PrintShape() << endl;
 
-    convOp->Execute();
+    convOp->ExecuteForward();
 
     cout << "Executed " << endl;
 
