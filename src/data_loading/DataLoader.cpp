72e76d99bc016225ea108f7db200a7e17112cc5f
chris <christopher.t.bate1@gmail.com>
2019-09-23 14:41:44 -0600

    Cleanup data loader api

--- src/data_loading/DataLoader.cpp
+++ src/data_loading/DataLoader.cpp
@@ -7,21 +7,19 @@
 using namespace DLFS;
 using namespace std;
 
-DataLoader::DataLoader(const std::string &examples_path)
+DataLoader::DataLoader(const std::string &dataset_path)
 {
-    m_exampleSource.Init(examples_path);
-    m_batchIndex = m_exampleIndex = 0;
-    m_totalBatches = m_exampleSource.GetNumExamples() / m_batchSize;
-
-    LOG.INFO() << "ExampleSource " << examples_path << ", " << m_exampleSource.GetNumExamples()
-               << " examples";
+    if (dataset_path.size() > 0)
+    {
+        LoadDataset(dataset_path);
+    }
 }
 
 DataLoader::~DataLoader()
 {
 }
 
-void DataLoader::GetNextBatch()
+void DataLoader::RunOnce()
 {
     // New data
     TensorPtr<uint8_t> imgBatchTensor = std::make_shared<Tensor<uint8_t>>("ImageBatchTensor");
@@ -34,38 +32,49 @@ void DataLoader::GetNextBatch()
     vector<vector<uint32_t>> catIds(m_batchSize);
     vector<uint32_t> widths(m_batchSize);
     vector<uint32_t> heights(m_batchSize);
-
     TensorShapeList boxShape(m_batchSize);
+    uint32_t maxWidth = 0;
+    uint32_t maxHeight = 0;
+
     for (unsigned int i = 0; i < m_batchSize; i++)
     {
-        const Example *ex = m_exampleSource.GetExample(m_exampleIndex);
+        LOG.DEBUG() << "Loading example " << i;
+        const Example *ex = nullptr;
+        {
+            std::lock_guard<std::mutex> lk(m_exSrcMutex);
+            ex = m_exampleSource.GetExample(m_exampleIndex);
+            m_exampleIndex = (m_exampleIndex + 1) % m_exampleSource.GetNumExamples();
+        }
+
+        LOG.DEBUG() << "Loading image " << i;
 
         // Check if the example has an image array, if so, load it.
         if (ex->image() != NULL)
-        {            
+        {
             imgBufs[i].assign(ex->image()->begin(), ex->image()->end());
+        }
+        else if (m_dataSource.get() != nullptr)
+        {
+            m_dataSource->GetBlob(ex->file_name()->str(), imgBufs[i]);
         }
         else
         {
-            if(!m_useFileSource){
-                throw std::runtime_error("DataLoader was not configured with a DataSource, but .db does not contain images.");
-            }            
-            m_dataSource.GetBlob(ex->file_name()->str(), imgBufs[i]);
+            throw std::runtime_error("Example did not have an image, and no data source specified.");
         }
 
         unsigned int numAnn = ex->annotations()->Length();
 
         widths[i] = ex->width();
         heights[i] = ex->height();
+        maxWidth = std::max(maxWidth, widths[i]);
+        maxHeight = std::max(maxHeight, heights[i]);
 
         LOG.DEBUG() << "Image " << ex->id() << " has " << numAnn << " annotations";
 
         boxShape.SetShape(i, {1, (int)numAnn, 4, 1});
 
-        m_exampleIndex = (m_exampleIndex + 1) % m_exampleSource.GetNumExamples();
-
         for (auto ann : *ex->annotations())
-        {            
+        {
             // Add the cat id.
             catIds[i].push_back((uint16_t)ann->cat_id());
             LOG.DEBUG() << "Found ann with cat id: " << ann->cat_id();
@@ -87,11 +96,11 @@ void DataLoader::GetNextBatch()
 
     bboxBatchTensor->AllocateIfNecessary();
     catIdBatchTensor->AllocateIfNecessary();
-    
+
     LOG.DEBUG() << "Building batch bbox tensor: " << bboxBatchTensor->PrintShape();
     unsigned int idx = 0;
     for (auto devPtr : bboxBatchTensor->GetIterablePointersOverBatch())
-    {        
+    {
         checkCudaErrors(cudaMemcpy(devPtr, boxes[idx].data(),
                                    boxes[idx].size() * sizeof(float), cudaMemcpyHostToDevice));
         idx++;
@@ -100,7 +109,7 @@ void DataLoader::GetNextBatch()
     idx = 0;
     LOG.DEBUG() << "Building batch catId tensor: " << catIdBatchTensor->PrintShape();
     for (auto devPtr : catIdBatchTensor->GetIterablePointersOverBatch())
-    {        
+    {
         checkCudaErrors(cudaMemcpy(devPtr, catIds[idx].data(),
                                    catIds[idx].size() * sizeof(uint16_t), cudaMemcpyHostToDevice));
         idx++;
@@ -108,23 +117,22 @@ void DataLoader::GetNextBatch()
 
     // Load the images onto the device buffer.
     // either decode jpegs or directly copy
-    if(m_useJpegDecoder){
+    if (m_useJpegDecoder)
+    {
         m_imgLoader.DecodeJPEG(imgBufs, imgBatchTensor);
-    } else {        
-        uint32_t maxWidth = 0;
-        uint32_t maxHeight = 0;
-        for(unsigned i = 0; i < widths.size(); i++){
-            maxWidth = std::max(maxWidth, widths[i]);
-            maxHeight = std::max(maxHeight, heights[i]);
-        }
-        LOG.DEBUG() << "Allocating image with shape " << maxHeight << " " <<  maxWidth;
-        imgBatchTensor->SetShape(m_batchSize, maxWidth, maxHeight, 1);        
-        imgBatchTensor->AllocateIfNecessary(); 
+    }
+    else
+    {
+        LOG.DEBUG() << "Allocating image with shape " << maxHeight << " " << maxWidth;
+        imgBatchTensor->SetShape(m_batchSize, maxWidth, maxHeight, 1);
+        imgBatchTensor->AllocateIfNecessary();
         imgBatchTensor->CopyBatchBuffersToDevice(imgBufs);
     }
 
-    checkCudaErrors(cudaDeviceSynchronize());    
+    checkCudaErrors(cudaDeviceSynchronize());
 
     // Push into queue
     m_batchesReady.emplace(std::make_tuple(imgBatchTensor, bboxBatchTensor, catIdBatchTensor));
+
+    m_batchIndex = (m_batchIndex + 1) % m_totalBatches;
 }
--- src/data_loading/DataLoader.hpp
+++ src/data_loading/DataLoader.hpp
@@ -13,10 +13,12 @@
 */
 #pragma once
 
+#include <mutex>
 #include <string>
 #include <vector>
 #include <fstream>
 #include <queue>
+#include <thread>
 #include <tuple>
 
 #include "LocalSource.hpp"
@@ -35,20 +37,43 @@ typedef std::array<float, 4> BBoxArray;
 class DataLoader
 {
 public:
-    DataLoader(const std::string &examples_path);
+    DataLoader(const std::string &dataset_path = "");
     ~DataLoader();
-    
-    void SetFileSource(const std::string &data_src)
+
+    /**
+     * LoadDataset
+     * Loads a dataset flatbuffer db from a specific path
+     */
+    void LoadDataset(const std::string &dataset_path)
     {
-        m_useFileSource = true;
-        m_dataSource.SetDirectory(data_src);
+        m_exampleSource.Init(dataset_path);
+        m_batchIndex = m_exampleIndex = 0;
+        m_totalBatches = m_exampleSource.GetNumExamples() / m_batchSize;
+
+        LOG.INFO() << "Dataset: " << dataset_path << ", " << m_exampleSource.GetNumExamples()
+                   << " examples found";
     }
 
-    void GetNextBatch();
-    
-    inline void SetUseJpegDecoder(bool val){
-        m_useJpegDecoder = val;
+    /**
+     * Set data source path 
+     * 
+     * Tells the m_dataSource where to find files.
+     * 
+     * The data source is only used if this is specified, otherwise we look for imags 
+     * in the datasets db.
+     */
+    void SetDataSourcePath(const std::string &data_src_path)
+    {
+        if(!m_dataSource.get()){
+            m_dataSource = std::make_unique<LocalSource>();
+        }
+        m_dataSource->SetPath(data_src_path);
     }
+
+    /**
+     * Loads a single batch into the queue
+     */
+    void RunOnce();
 
     inline void SetBatchSize(unsigned int batchSize)
     {
@@ -61,26 +86,49 @@ public:
         return m_batchSize;
     }
 
+    /**
+     * Returns total number of BATCHES
+     */
     inline int GetLength()
     {
         return m_totalBatches;
     }
 
+    /**
+     * Prints log messages for 
+     * current batch, and statistics (time per batch, queue size, etc.)
+     */
     inline void Summary()
     {
         LOG.INFO() << "DATA LOADER:";
-        LOG.INFO() << " Curr. Batch: " << m_batchIndex << "/" << m_totalBatches;
+        LOG.INFO() << "--Curr. Batch: " << m_batchIndex << "/" << m_totalBatches;
+        LOG.INFO() << "--Queued Batches: " << m_batchesReady.size();
     }
 
+    /**
+     * Prefetch Queue size
+     */
     inline void SetPrefetch(unsigned int prefetchLimit)
     {
         m_prefetchLimit = prefetchLimit;
     }
 
-    ObjDetExampleBatch DequeBatch(){
+    /**
+     * Returns next batch
+     */
+    ObjDetExampleBatch DequeBatch()
+    {
         ObjDetExampleBatch batch = m_batchesReady.front();
         m_batchesReady.pop();
         return batch;
+    }
+
+    /**
+     * Controls Image Decoding
+     */
+    void EnableJpegDecoder()
+    {
+        m_useJpegDecoder = true;
     }
 
 private:
@@ -98,11 +146,17 @@ private:
     // The limit on the size of m_batchesReady
     unsigned int m_prefetchLimit{1};
 
-    LocalSource m_dataSource;
     ExampleSource m_exampleSource;
+    std::mutex m_exSrcMutex;
+
     ImageLoader m_imgLoader;
-    bool m_useFileSource{false};
     bool m_useJpegDecoder{false};
+
+    std::unique_ptr<DataSource> m_dataSource{nullptr};
+
+    /**
+     * Threading
+     */
 
     // The set of batches which are already loaded.
     std::queue<ObjDetExampleBatch> m_batchesReady;
