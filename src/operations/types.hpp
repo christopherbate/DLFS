2680d52db92beca7292e94eb5a074faf299af061
Christopher Bate <christopher.t.bate1@gmail.com>
2019-12-02 01:30:02 -0700

    Update custom types

--- src/operations/types.hpp
+++ src/operations/types.hpp
@@ -1,15 +1,18 @@
 #pragma once
 #include <array>
 
-namespace DLFS
-{
-typedef std::array<int, 2> Pad2d;
-typedef std::array<int, 2> Stride2d;
+namespace DLFS {
+using Pad2D = std::array<int, 2>;
+using Stride2D = std::array<int, 2>;
 
-enum CustomOpDataType
-{
-    Float,
-    Double,
-    Half
-};
+// Width x Height
+using Filter2D = std::array<int, 2>;
+
+constexpr Pad2D Pad0 = Pad2D{0, 0};
+constexpr Pad2D Pad1 = Pad2D{1, 1};
+constexpr Pad2D Pad2 = Pad2D{2, 2};
+constexpr Stride2D Stride1 = Stride2D{1, 1};
+constexpr Stride2D Stride2 = Stride2D{2, 2};
+
+enum CustomOpDataType { Float, Double, Half };
 } // namespace DLFS
